4836be9d412fe23c2cbd0565d92cc72be7a377bc
Jakub <Jakub.klik6@gmail.com>
2023-02-02 22:29:22 +0100

    farm generator and buyingFarm function

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
-import org.w3c.dom.ls.LSOutput;
+import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
 
-        //UserWelcome();
+        UserWelcome();
 
-        FarmGenerator();
-        Farm farm = new Farm(1.1,1.1,1);
+        Player player1 = new Player();
+
+        System.out.println("Now you can decide which farm you want to buy: \n");
+
+        Farm farm1 = generateRandomObject();
+        Farm farm2 = generateRandomObject();
+        Farm farm3 = generateRandomObject();
+
+        System.out.println("Farm 1" + farm1.toString());
+        System.out.println("Farm 2" + farm2.toString());
+        System.out.println("Farm 3" + farm3.toString());
+
+        buyingFarm();
 
 
 
@@ -18,17 +28,50 @@ public class Main {
     {
         System.out.println("Hello and welcome to the farm simulator!");
 
-        System.out.println("Please enter your name: ");
+        System.out.print("Please enter your name: ");
         Scanner userInput = new Scanner(System.in);
         String userName = userInput.nextLine();
-        System.out.println("Hello " + userName);
-    }
+        System.out.println("\nHello " + userName);
 
-    public static void FarmGenerator()
-    {
-        Farm farm1 = new Farm();
-        Farm farm2 = new Farm();
-        Farm farm3 = new Farm();
 
     }
+
+    public static void buyingFarm(){
+        System.out.print("\nPress 1,2 or 3 to buy farm!: ");
+
+        Scanner userInput = new Scanner(System.in);
+        String numberInput = userInput.nextLine();
+
+        int num = Integer.parseInt(numberInput);
+
+        while(true)
+        {
+            if(num == 1) {
+                System.out.println("You have bought farm number 1!");
+                break;
+            }
+            if(num == 2) {
+                System.out.println("You have bought farm number 2!");
+                break;
+            }
+            if(num==3){
+                System.out.println("You have bought farm number 3!");
+                break;
+            }
+            else
+            {
+                System.out.println("You have entered invalid value!");
+                continue;
+            }
+        }
+    }
+
+    public static Farm generateRandomObject() {
+        Random random = new Random();
+        double x = random.nextInt(500);
+        int y = random.nextInt(5);
+        return new Farm(x, y);
+    }
+
+
 }
